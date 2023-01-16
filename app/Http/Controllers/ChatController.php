2db4a438ad30f828bab9a08330286d65c24b2bb9
1727003855 <ejc.damian@yavirac.edu.ec>
2023-01-16 09:02:59 -0500

    fix bug chat

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Events\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ChatController extends Controller
 {
     public function message(Request $request){
-        event(New Message($request->input('username'), $request->input('message')));
+        $user= Auth::user();
+        $username= $user->name;
+        event(New Message($username, $request->input('message')));
         return[];
     }
 }
